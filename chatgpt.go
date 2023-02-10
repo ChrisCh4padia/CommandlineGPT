8b64106c1742b5b4112fdce4d36c44a63e9cba51
Your Name <christof-renner@t-online.de>
2023-02-10 21:29:05 +0100

    Inital release

--- chatgpt.go
+++ chatgpt.go
@@ -1,32 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strings"
-	"time"
-
-	"github.com/solywsh/chatgpt"
 )
 
-// prompt is the user's input text
 var prompt string
-
-// response to request
 var answer string
-
-// API key for the GPTservice
 var apikey string
-
 var err error
 
 func main() {
-	// Define command-line flags for setting the prompt and API key
+
 	flag.StringVar(&prompt, "p", "", "Your chatgpt prompt")
 	flag.StringVar(&apikey, "k", "", "Your chatgpt API key")
 	flag.Parse()
 
-	// Check that the prompt is not empty
+	if apikey == "" {
+		envvar := os.Getenv("gptkey")
+		if envvar == "" {
+			fmt.Println("Error: No API key specified. Please set the key through the -k flag or the gptkey environment variable")
+			return
+		} else {
+			apikey = envvar
+		}
+	}
+
 	if prompt != "" {
 		generateGPTmessage()
 		fmt.Println(answer)
@@ -36,20 +41,31 @@ func main() {
 }
 
 func generateGPTmessage() {
-	// The timeout is used to control the situation that the session is in a long and multi session situation.
-	// If it is set to 0, there will be no timeout. Note that a single request still has a timeout setting of 30s.
-	chat := chatgpt.New(apikey, "user_id(not required)", 0*time.Second)
-	defer chat.Close()
 
-	// Get the response
-	answer, err = chat.Chat(prompt)
-	if err != nil {
-		if strings.Contains(err.Error(), "401") {
-			fmt.Printf("Unauthorized. Please check your key")
-		} else {
-			fmt.Println(err)
-		}
-
+	body := map[string]interface{}{
+		"prompt":     prompt,
+		"model":      "text-davinci-003",
+		"max_tokens": 100,
 	}
+	jsonBody, _ := json.Marshal(body)
 
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "https://api.openai.com/v1/completions", bytes.NewBuffer(jsonBody))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apikey))
+
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	var response map[string]interface{}
+	json.Unmarshal(respBody, &response)
+
+	choices := response["choices"].([]interface{})
+	choice := choices[0].(map[string]interface{})
+	text := choice["text"].(string)
+
+	text = strings.TrimSpace(text)
+
+	fmt.Println(text)
 }
